5c272e039d42d3baf45f4758463e2ef501eb3db9
광부입니다 <kwy1379@ajou.ac.kr>
2025-11-19 04:32:04 +0900

    mTLS는 로그인을 위한 로직이 아니라 인증 로직이기에 successHandler를 추가하면서 오류가 발생했던 것임 -> service Layer에서 반환하도록 수정하였음. (SpringSecurity와 Spring에서 내부적으로 사용하는 Authenticate 라이브러리 이름이 jakarta/javax로 달라서 오류 발생.. mTls -> Jwt를 스프링 시큐리티에서 한번에 처리하는 것을 권장하지 않았던 이유는 mTls가 로그인 용도가 아니기 때문.)
    SpringSecurity의 인증처리 로직을 Customizer로 분리(mTLS 인증 필수, HTTPS+Jwt, Public)
    + Customizer는 함수형 인터페이스라 throws를 람다식에서 처리해야됨. 그런데 대부분의 람다식에서 컴파일단계 오류가 있어서, 콜백함수 Wrapper로 예외 타입을 변경하고 Filter로 던지도록 수정.
    Jwt의 관리는 Oauth2ResourceServer로 처리하니 간편함.

--- demo/src/main/java/com/example/demo/config/MutualTlsSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/MutualTlsSecurityConfig.java
@@ -1,48 +1,35 @@
-package com.example.demo.config;
-
-import com.example.demo.controller.VehicleController;
-import com.example.demo.filter.FixX509AttributeFilter;
-import com.example.demo.filter.LoggingFilter;
-import com.example.demo.service.VehicleDetailsService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter;
-
-@Configuration
-@Order(1)
-@RequiredArgsConstructor
-public class MutualTlsSecurityConfig {
-    private final VehicleDetailsService vehicleDetailsService;
-
-    @Bean
-    public SecurityFilterChain mtlsFilterChain(HttpSecurity http) throws Exception {
-
-        http
-                .securityMatcher("/auth/token") // 1. 이 필터체인은 /auth/token 경로에만 적용하도록 활성화
-                // FixX509AttributeFilter가 가장 먼저 실행되어 인증서 속성 문제를 해결합니다.
-                .addFilterBefore(new FixX509AttributeFilter(), X509AuthenticationFilter.class)
-                .addFilterAfter(new LoggingFilter(), FixX509AttributeFilter.class) // LoggingFilter는 그 다음에 실행됩니다.
-                .csrf(csrf -> csrf.disable())
-                .x509(x509 -> x509
-                        .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
-                        .userDetailsService(vehicleDetailsService)
-                        // 인증 상세 정보로 CustomAuthDetails 객체를 생성하도록 지정
-                        .authenticationDetailsSource(CustomAuthDetails::new)
-                )
-                .sessionManagement(session -> session
-                        // REST API이므로 세션을 STATELESS로 설정
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated() // securityMatcher로 이미 경로가 한정되었으므로, 모든 요청에 인증 요구
-                );
-
-        System.out.println(">>> Mtls filter chain active");
-        return http.build();
-    }
-}
+//package com.example.demo.config;
+//
+//import com.example.demo.filter.FixX509AttributeFilter;
+//import com.example.demo.filter.LoggingFilter;
+//import com.example.demo.service.VehicleDetailsService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter;
+//
+//@Configuration
+//@Order(1)
+//@RequiredArgsConstructor
+//public class MutualTlsSecurityConfig {
+//    private final VehicleDetailsService vehicleDetailsService;
+//
+//    @Bean
+//    public SecurityFilterChain mtlsFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .securityMatcher("/auth/token")
+//                .apply(new CommonHttpConfigurer()).and()
+//                .addFilterBefore(new FixX509AttributeFilter(), X509AuthenticationFilter.class)
+//                .addFilterAfter(new LoggingFilter(), FixX509AttributeFilter.class)
+//                .x509(x509 -> x509
+//                        .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+//                        .userDetailsService(vehicleDetailsService)
+//                        .authenticationDetailsSource(CustomAuthDetails::new));
+//
+//        System.out.println(">>> Mtls filter chain active");
+//        return http.build();
+//    }
+//}
